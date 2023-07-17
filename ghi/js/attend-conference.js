9e862cd384725b85daa09fa6c1782d03a9d54ca4
Ellie <hello@ellietiglao.com>
2023-07-17 17:34:47 -0400

    Push form data to microservice database at api/attendees/.

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -6,19 +6,47 @@ window.addEventListener('DOMContentLoaded', async () => {
     if (response.ok) {
       const data = await response.json();
 
-      for (let conference of data.conferences) {
-        const option = document.createElement('option');
-        option.value = conference.href;
-        option.innerHTML = conference.name;
-        selectTag.appendChild(option);
-      }
+        for (let conference of data.conferences) {
+            const option = document.createElement('option');
+            option.value = conference.href;
+            option.innerHTML = conference.name;
+            selectTag.appendChild(option);
+        }
 
-      // Here, add the 'd-none' class to the loading icon
-          let div = document.querySelector(".spinner-grow");
-          div.classList.add("d-none");
-      // Here, remove the 'd-none' class from the select tag
-          let select = document.querySelector("#conference");
-          select.classList.toggle("d-none");
+        let div = document.querySelector(".spinner-grow");
+        div.classList.add("d-none");
+        let select = document.querySelector("#conference");
+        select.classList.toggle("d-none");
+
+        const formTag = document.getElementById("create-attendee-form");
+        formTag.addEventListener("submit", async event => {
+            event.preventDefault();
+            const formData = new FormData(formTag);
+            const json = JSON.stringify(Object.fromEntries(formData));
+            const attendeeUrl = 'http://localhost:8001/api/attendees/';
+
+            const fetchConfig = {
+                method: "post",
+                body: json,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+            const response = await fetch(attendeeUrl, fetchConfig);
+            if (response.ok) {
+                const newAttendee = await response.json();
+                console.log("new attendee:", newAttendee);
+                let div = document.querySelector("#success-message");
+                div.classList.remove("d-none");
+                let form = document.querySelector("#create-attendee-form");
+                form.classList.add("d-none")
+            }
+
+        });
+
+
     }
+
+
 
   });
