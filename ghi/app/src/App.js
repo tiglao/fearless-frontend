7f087429c0a693e657e21f81eabeb00ea1c9a7d7
Ellie <hello@ellietiglao.com>
2023-07-18 15:24:10 -0400

    Set app conditions for SPA refactor.

---
+++ ghi/app/src/App.js
@@ -0,0 +1,18 @@
+import Nav from './Nav';
+import AttendeesList from './AttendeesList';
+
+function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
+  return (
+    <>
+      <Nav />
+      <div className="container">
+        <AttendeesList attendees={props.attendees} />
+      </div>
+    </>
+  );
+}
+
+export default App;
