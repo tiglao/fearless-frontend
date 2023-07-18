66f67e881a79ece7f70022be7a79c98e4703489d
Ellie <hello@ellietiglao.com>
2023-07-17 22:00:01 -0400

    Integrate bootstrap into code.

--- ghi/attendees/src/App.js
+++ ghi/attendees/src/App.js
@@ -1,29 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
 
 function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-          {props.attendees && props.attendees.map(attendee => {
-            return (
-              <tr key = {attendee.href}>
++    <Nav />
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Conference</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* for (let attendee of props.attendees) {
+              <tr>
                 <td>{ attendee.name }</td>
                 <td>{ attendee.conference }</td>
               </tr>
-            );
-          })}
-        </thead>
-        <tbody>
-
-        </tbody>
-      </table>
-    </div>
+            } */}
+            {props.attendees.map(attendee => {
+              return (
+                <tr key={attendee.href}>
+                  <td>{ attendee.name }</td>
+                  <td>{ attendee.conference }</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
   );
 }
 
